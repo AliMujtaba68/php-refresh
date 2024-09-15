5f320a190c4691d88d6a8cf236e5339c72d8c8c4
Alimujtaba68 <a.mujtaba068@gmail.com>
2024-09-15 23:33:06 +0500

    added arrays content

---
+++ Day_1/arrays.php
@@ -0,0 +1,12 @@
+<?php 
+
+$list = array("id" => 1, "name" => "Ali", "age" => 25);
+$countries = ["Australia", "Pakistan", "India", "USA"]; 
+
+// Output with line breaks for CLI
+echo $list["name"] . "\n";
+echo $countries[2] . "\n";
+
+
+echo count($countries) . "\n";
+
